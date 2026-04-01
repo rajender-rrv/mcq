e93503027d4581886f69512aec83356475ec8dd8
Rajender <rajenderreddy.rrv@gmail.com>
2026-04-01 11:26:20 +0530

    Updated access token expire time to 1hr

--- back-end/mcq-app/app/core/config.py
+++ back-end/mcq-app/app/core/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
         min_length=32,
     )
     JWT_ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     class Config:
